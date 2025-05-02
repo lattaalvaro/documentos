fc346b5985acd96b2abd374c7db9a9d3cdb39e31
lattaalvaro <latta665@gmail.com>
2025-05-01 22:25:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -185,9 +185,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     app.run(debug=True)
